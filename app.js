1c4c634a0f362915b48da46b67a41cebfd0d134e
Tiago <tiagosantiniathayde@outlook.com>
2021-01-06 20:39:04 -0300

    fix linting

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -10,15 +10,15 @@ const app = express();
 
 // depends on enviroment variables
 console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware! 👋👋");
+  console.log('Hello from the middleware! 👋👋');
   next();
 });
 
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
