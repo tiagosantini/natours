b8e64ffcd83e22724deb152f763018f09e825903
Tiago <tiagosantiniathayde@outlook.com>
2021-01-07 16:38:53 -0300

    add new "slug" field and related hook to tourModel.js
    "slug" field accepts String
    tourSchema now runs a pre save hook that inserts a slugified version of the "name" field into "slug"

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // exports the constructor for the Tour mongodb model
 
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -64,6 +66,12 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
