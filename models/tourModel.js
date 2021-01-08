fcbfd27a8179407df9904e1406db49c2d154a955
Tiago <tiagosantiniathayde@outlook.com>
2021-01-08 13:32:32 -0300

    add "secretTour" field in the Tour schema...
    secretTour is used by pre all 'find' query middleware to keep
    documents that have it set as false (default) hidden.

--- models/tourModel.js
+++ models/tourModel.js
@@ -57,6 +57,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -74,6 +78,20 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+  // tourSchema.pre('find', function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
