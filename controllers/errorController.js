e91fa287307528f68c8d4ddeb033c2450597b536
Tiago <tiagosantiniathayde@outlook.com>
2021-01-12 15:57:25 -0300

    add error handler for duplicate fields in database
    handleDuplicateFieldsDB() will return a 400 status with a message displaying which value is causing the error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,13 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  // const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  // console.log(value);
+  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -44,6 +51,8 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     if (err.name === 'CastError') error = handleCastErrorDB(error);
+
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
 
     sendErrorProd(error, res);
   }
