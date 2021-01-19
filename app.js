9461deec064cfa8011283d478b1fda32f67d9701
Tiago <tiagosantiniathayde@outlook.com>
2021-01-19 10:42:31 -0300

    add implementation of rate limiting
    The express-rate-limit package is now added to the project.
    
    app.js uses express-rate-limit to prevent brute-force and DDOS attacks by limiting requests to 100 in an hour, this value can be changed in the limiter object

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,11 +11,19 @@ const app = express();
 
 // middleware hell
 
-// depends on enviroment variables
+// 1) Global Middleware
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour.',
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
