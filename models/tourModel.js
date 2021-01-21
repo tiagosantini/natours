572e1c99cecbeb98e5042644e8438bcec1a44cc1
Tiago <tiagosantiniathayde@outlook.com>
2021-01-21 15:06:12 -0300

    add populate pre-hook to guides
    mongoose will populate the "guides" field with the referenced user data using a pre-find middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -144,6 +144,14 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+  next();
+});
+
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
